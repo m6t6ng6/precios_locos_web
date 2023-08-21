a0014458c2d462382ea4eb854c51237633b9a038
Fernando Bustamante <m6t6ng6@m6t6ng6-MacBookPro-M1-MAX-Deluxe.local>
2023-08-21 13:12:34 -0300

    MVP

---
+++ flask_app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for, redirect
+import re
+
+app = Flask(__name__)
+
+nombre_de_empresa = "Precios Locos"
+telefono = "0054 (911) 5345-7787"     # formato: 0054 (911) 5345-7787
+pattern = r'(\d)'
+telefono_link = ''.join(re.findall(pattern, telefono))
+
+lista_de_precios="https://docs.google.com/spreadsheets/d/1To91GePxRf-gTOqdDhFL_-ZIoMgvWOQMMdCi2nhLtFQ/edit?usp=drive_link"
+
+@app.route("/")
+def home():
+    global nombre_de_empresa
+    global telefono
+    return render_template(
+        "productos.html",
+        titulo=nombre_de_empresa,
+        mensaje1="Todas las publicaciones que ves en ML las podés obtener a precio de descuento pagando en efectivo o por transferencia.",
+        mensaje2="Contactanos a nuestro WhatsApp: " + telefono + ". Clickeá sobre el ícono de WhatsApp en la barra de navegación para agregarnos.",
+        envio="El envío se realiza a convenir entre nosotros y vos en algún lugar público para la seguridad de todos.",
+        telefono=telefono,
+        telefono_link=telefono_link,
+        bienvenida="Bienvenido a la Web Oficial de Precios Locos",
+        medios_de_pago="Aceptamos MercadoPago, tarjetas (crédito y débito), transferencias y efectivo",
+        envios="Enviamos a todo el pais",
+        precios_comentarios="Lista de precios de celulares de alta y media gama - consultar stock",
+        lista_de_precios=lista_de_precios
+        )
+
+if __name__ == "__main__":
+    app.run(debug=True)
